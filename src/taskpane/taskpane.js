32aea53b905f8580e845d5721ccfc18f04c82616
pdpAddl <patrick.schaible1@gmail.com>
2023-02-26 11:31:23 +0100

    Select and copy text

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -19,8 +19,21 @@ export async function run() {
      * Insert your Word code here
      */
 
-    // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+    var rangeSelected;
+    var strSelected;
+
+    // Get Selected Range
+    rangeSelected = context.document.getSelection();
+
+    // Load selected string
+    rangeSelected.load("text");
+
+    // Wait until everything is synced
+    await context.sync();
+
+    // extract string to variable for further processing
+    strSelected = rangeSelected.text;
+    const paragraph = context.document.body.insertParagraph(strSelected, Word.InsertLocation.end);
 
     // change the paragraph color to blue.
     paragraph.font.color = "blue";
