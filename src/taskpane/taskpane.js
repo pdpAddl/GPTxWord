b6ce50833333ffdda99faa50e3f9f03232bb3963
pdpAddl <patrick.schaible1@gmail.com>
2023-03-02 15:11:32 +0100

    Revert "Merge branch 'pAddl_Testing'"
    This reverts commit 2dc68a83bd738a0e7d509f7730eef83a26203f74, reversing
    changes made to 612de2c5fc52e755defeb42b973b0f9915927d0e.

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -9,65 +9,18 @@ Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run1").onclick = run1;
-    document.getElementById("run2").onclick = run2;
+    document.getElementById("run").onclick = run;
   }
 });
 
-export async function run1() {
+export async function run() {
   return Word.run(async (context) => {
     /**
      * Insert your Word code here
      */
 
-    var rangeSelected;
-    var strSelected;
-
-    // Get Selected Range
-    rangeSelected = context.document.getSelection();
-
-    // Load selected string
-    rangeSelected.load("text");
-
-    // Wait until everything is synced
-    await context.sync();
-
-    // extract string to variable for further processing
-    strSelected = rangeSelected.text;
-
-    // Insert string at the end of the paragraph
-    const paragraph = context.document.body.insertParagraph(strSelected, Word.InsertLocation.end);
-
-    // change the paragraph color to blue.
-    paragraph.font.color = "blue";
-
-    await context.sync();
-  });
-}
-
-export async function run2() {
-  return Word.run(async (context) => {
-    /**
-     * Insert your Word code here
-     */
-
-    var rangeSelected;
-    var strSelected;
-
-    // Get Selected Range
-    rangeSelected = context.document.getSelection();
-
-    // Load selected string
-    rangeSelected.load("text");
-
-    // Wait until everything is synced
-    await context.sync();
-
-    // extract string to variable for further processing
-    strSelected = rangeSelected.text;
-
-    // Insert string at the end of the paragraph
-    const paragraph = context.document.body.insertParagraph(strSelected, Word.InsertLocation.start);
+    // insert a paragraph at the end of the document.
+    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
 
     // change the paragraph color to blue.
     paragraph.font.color = "blue";
