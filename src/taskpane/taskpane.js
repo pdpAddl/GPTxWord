878827764eee1b120c1d2a6a247d21b3523f4510
pdpAddl <patrick.schaible1@gmail.com>
2023-03-03 18:13:13 +0100

    Added first functions for text correction and text completion
    - Only replacement text is used
    - Changed/Added text gets documented by footprints

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -9,22 +9,70 @@ Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    //document.getElementById("BtnAddText").onclick = addTextToSelection;
+    document.getElementById("BtnCorrectText").onclick = correctSelection;
   }
 });
 
-export async function run() {
+export async function addTextToSelection() {
   return Word.run(async (context) => {
     /**
      * Insert your Word code here
      */
 
-    // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+    var rangeSelected;
+    var selectedText;
+    var addedText;
 
-    // change the paragraph color to blue.
-    paragraph.font.color = "blue";
+    // Get Selected Range
+    rangeSelected = context.document.getSelection();
+
+    // Load selected string
+    rangeSelected.load("text");
+
+    // Wait until everything is synced
+    await context.sync();
+
+    // extract string to variable for further processing
+    selectedText = rangeSelected.text;
+
+    // TODO: Add Text via GPT API
+    addedText = " TEST ";
+
+    // Insert string at the end of the selected area
+    rangeSelected.insertText(addedText, Word.InsertLocation.end);
+    rangeSelected.insertFootnote("Parts of that text were added by the GPT AI");
+
+    // Insert added text with foot note
 
     await context.sync();
   });
 }
+
+export async function correctSelection() {
+  return Word.run(async (context) => {
+    /**
+     * Insert your Word code here
+     */
+
+    var rangeSelected;
+    var correctedText;
+
+    // Get Selected Range
+    rangeSelected = context.document.getSelection();
+    rangeSelected.clear();
+
+    // Wait until everything is synced
+    await context.sync();
+
+    // Correct Text via GPT API - TODO
+    correctedText = "abcdefg 12345 doijdas dasadssd asdafsds";
+
+    // Insert corrected text with foot note
+    rangeSelected.insertText(correctedText, Word.InsertLocation.start);
+    rangeSelected.insertFootnote("This text was corrected by the GPT AI");
+
+    await context.sync();
+  });
+}
+
