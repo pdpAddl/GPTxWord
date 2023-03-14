3499590162886f82bdcaf21aa5b8a9b90fa20037
SkyLuzifer <125306707+SkyLuzifer@users.noreply.github.com>
2023-03-14 14:21:30 +0100

    Added new functions
    Added image_generation function
    Added text_translation function

--- src/taskpane/GPT_API.js
+++ src/taskpane/GPT_API.js
@@ -5,50 +5,83 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-/*
+
 async function text_completion ( API, text){
   const response = await API.createCompletion({
-    model: "text-davinci-003",     //es existieren verschieden Modelle des GPT davinci003 max request 4000 tokens, beste Qualität
-    //model: "gpt-3.5-turbo-0301",      //gpt-3.5-turbo
-    prompt: "Complete this text and keep the original language:" + text,
-    temperature: 0,
-    max_tokens: 100,
+    model: "gpt-3.5-turbo", //es existieren verschieden Modelle des GPT davinci003 max request 4000 tokens, beste Qualität
+    messages: [
+      { role: "system", content: "du antwortest sachlich" },
+      { role: "user", content: "Complete this text and keep the original Language: " + text },
+    ],
   });
 
   console.log("Anfrage: Complete this Text and keep the original Language: "+ text)
-  console.log("Antwort: "+response.data.choices[0].text);
+  console.log("Antwort: "+response.data.choices[0].message.content);
   console.log("Anfrage ID: "+response.data.id); 
   console.log("Tokens für Anfrage: "+response.data.usage.prompt_tokens);
   console.log("Tokens für Antwort: "+response.data.usage.completion_tokens);
   console.log("Insgesamt verwendete Token: "+response.data.usage.total_tokens);
   return  response;
 }
-*/
-TEXT = "ich wollte nur";
 
-//Antwort = text_completion(openai, TEXT);
+
 
 async function text_correction(API, text) {
   const response = await API.createChatCompletion({
     model: "gpt-3.5-turbo", //es existieren verschieden Modelle des GPT davinci003 max request 4000 tokens, beste Qualität
     messages: [
-      { role: "system", content: "du antwortest beleidigend" },
-      { role: "user", content: "Complete this text and keep the original Language: " + text },
+      { role: "system", content: "du antwortest sachlich" },
+      { role: "user", content: "Correct this text and keep the original Language: " + text },
     ],
   });
 
-  console.log("Anfrage: Complete this Text and keep the original Language: " + text);
-  //console.log("Antwort: "+response["data"]["choices"][0]["message"]["content"]);
+/*  console.log("Anfrage: Correct Spelling and Grammar of the following Text and keep the original Language: " + text);
   console.log("Antwort: " + response.data.choices[0].message.content);
   console.log("Anfrage ID: " + response.data.id);
   console.log("Tokens für Anfrage: " + response.data.usage.prompt_tokens);
   console.log("Tokens für Antwort: " + response.data.usage.completion_tokens);
   console.log("Insgesamt verwendete Token: " + response.data.usage.total_tokens);
-  console.log("Kosten: " + ((response.data.usage.total_tokens / 1000) * 0, 2) + " cent");
+  console.log("Kosten: " + ((response.data.usage.total_tokens / 1000) * 0, 2) + " cent");*/
   return response;
 }
 
-//Antwort = text_correction(openai, TEXT);
+async function text_correction(API, text) {
+  const response = await API.createChatCompletion({
+    model: "gpt-3.5-turbo", //es existieren verschieden Modelle des GPT davinci003 max request 4000 tokens, beste Qualität
+    messages: [
+      { role: "system", content: "du antwortest sachlich" },
+      { role: "user", content: "Correct this text and keep the original Language: " + text },
+    ],
+  });
+
+/*  console.log("Anfrage: Correct Spelling and Grammar of the following Text and keep the original Language: " + text);
+  console.log("Antwort: " + response.data.choices[0].message.content);
+  console.log("Anfrage ID: " + response.data.id);
+  console.log("Tokens für Anfrage: " + response.data.usage.prompt_tokens);
+  console.log("Tokens für Antwort: " + response.data.usage.completion_tokens);
+  console.log("Insgesamt verwendete Token: " + response.data.usage.total_tokens);
+  console.log("Kosten: " + ((response.data.usage.total_tokens / 1000) * 0, 2) + " cent");*/
+  return response;
+}
+
+async function text_translation(API, text, Language) {
+  const response = await API.createChatCompletion({
+    model: "gpt-3.5-turbo", 
+    messages: [
+      { role: "system", content: "You are a helpful assistant that translates to" + Language},
+      { role: "user", content: "Translate the following text to" + Language + ": " + text},
+    ],
+  });
+
+  //console.log("Anfrage: Correct Spelling and Grammar of the following Text and keep the original Language: " + text);
+  console.log("Antwort: " + response.data.choices[0].message.content);
+  //console.log("Anfrage ID: " + response.data.id);
+  //console.log("Tokens für Anfrage: " + response.data.usage.prompt_tokens);
+  //console.log("Tokens für Antwort: " + response.data.usage.completion_tokens);
+  //console.log("Insgesamt verwendete Token: " + response.data.usage.total_tokens);
+  //console.log("Kosten: " + ((response.data.usage.total_tokens / 1000) * 0, 2) + " cent");
+  return response;
+}
 
 async function key_validation(API) {
   try {
@@ -67,4 +100,23 @@ async function key_validation(API) {
   }
 }
 
-key_validation(openai);
+
+
+async function image_generation(API, description) {
+  const response = await API.createImage({
+    prompt : description,
+    n : 1,
+    size : "1024x1024"
+  })
+  image_url = await response.data.data[0].url;
+  console.log(image_url)
+}
+
+TEXT = "ich wollte nur";
+TEXT1 = "bonjour tu vas bien ou tu as des problèmes?";
+
+//Antwort = text_completion(openai, TEXT);
+//Antwort = text_correction(openai, TEXT);
+//text_translation(openai, TEXT1,"deutsch")
+//key_validation(openai);
+//image_generation(openai, null);
