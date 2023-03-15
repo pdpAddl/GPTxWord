ed9b168afaf7146e8fd6085ad253e87f1a591514
SkyLuzifer <125306707+SkyLuzifer@users.noreply.github.com>
2023-03-15 16:46:46 +0100

    Changed Funktion headers
    Deleted API from function headers

--- src/taskpane/GPT_API.js
+++ src/taskpane/GPT_API.js
@@ -1,11 +1,16 @@
 const { Configuration, OpenAIApi } = require("openai");
-//require("process");
 
-var key = process.env.OPENAI_API_KEY;
+var key = "";
 
-set_key(key);
+const configuration = new Configuration({
+  apiKey: key,
+});
+var currentOPENAIApi = new OpenAIApi(configuration);
+
+//set_key(key);
 
 async function set_key(newKey){
+  OldOpenAIApi = currentOPENAIApi;
   const NewConfiguration = new Configuration({
     apiKey: newKey,
   });
@@ -14,22 +19,23 @@ async function set_key(newKey){
 
   if (await key_validation(NewOpenAIApi)){
     console.log("Key correct");
-    return NewOpenAIApi
+    currentOPENAIApi = NewOpenAIApi;
+    return true;
   }else{
     console.log("Key incorrect");
-    return false;                     //return OldOpenAIApi ???
+    currentOPENAIApi = OldOpenAIApi;
+    return false;                   
   }
 }
 
 
-const configuration = new Configuration({
-  apiKey: key,
-});
 
-const openai = new OpenAIApi(configuration);
 
-async function text_completion ( API, text){
-  const response = await API.createCompletion({
+// const openai = new OpenAIApi(configuration);
+
+
+async function text_completion (text){
+  const response = await currentOPENAIApi.createChatCompletion({
     model: "gpt-3.5-turbo", //es existieren verschieden Modelle des GPT davinci003 max request 4000 tokens, beste Qualität
     messages: [
       { role: "system", content: "du antwortest sachlich" },
@@ -38,7 +44,7 @@ async function text_completion ( API, text){
   });
 
   // console.log("Anfrage: Complete this Text and keep the original Language: "+ text)
-  // console.log("Antwort: "+response.data.choices[0].message.content);
+   console.log("Antwort: "+response.data.choices[0].message.content);
   // console.log("Anfrage ID: "+response.data.id); 
   // console.log("Tokens für Anfrage: "+response.data.usage.prompt_tokens);
   // console.log("Tokens für Antwort: "+response.data.usage.completion_tokens);
@@ -50,8 +56,8 @@ async function text_completion ( API, text){
 
 
 
-async function text_correction(API, text) {
-  const response = await API.createChatCompletion({
+async function text_correction(text) {
+  const response = await currentOPENAIApi.createChatCompletion({
     model: "gpt-3.5-turbo", //es existieren verschieden Modelle des GPT davinci003 max request 4000 tokens, beste Qualität
     messages: [
       { role: "system", content: "du antwortest sachlich" },
@@ -59,9 +65,9 @@ async function text_correction(API, text) {
     ],
   });
 
-/*  console.log("Anfrage: Correct Spelling and Grammar of the following Text and keep the original Language: " + text);
+/*  console.log("Anfrage: Correct Spelling and Grammar of the following Text and keep the original Language: " + text);*/
   console.log("Antwort: " + response.data.choices[0].message.content);
-  console.log("Anfrage ID: " + response.data.id);
+ /* console.log("Anfrage ID: " + response.data.id);
   console.log("Tokens für Anfrage: " + response.data.usage.prompt_tokens);
   console.log("Tokens für Antwort: " + response.data.usage.completion_tokens);
   console.log("Insgesamt verwendete Token: " + response.data.usage.total_tokens);
@@ -70,8 +76,8 @@ async function text_correction(API, text) {
   return response;          //für den Text des Ergebnisses: response.data.choices[0].message.content
 }
 
-async function text_translation(API, text, Language) {
-  const response = await API.createChatCompletion({
+async function text_translation(text, Language) {
+  const response = await currentOPENAIApi.createChatCompletion({
     model: "gpt-3.5-turbo", 
     messages: [
       { role: "system", content: "You are a helpful assistant that translates to" + Language},
@@ -109,8 +115,8 @@ async function key_validation(API) {
 
 
 
-async function image_generation(API, description) {
-  const response = await API.createImage({
+async function image_generation(description) {
+  const response = await currentOPENAIApi.createImage({
     prompt : description,
     n : 1,
     size : "1024x1024"
@@ -124,8 +130,8 @@ async function image_generation(API, description) {
 TEXT = "ich wollte nur";
 TEXT1 = "bonjour tu vas bien ou tu as des problèmes?";
 
-//Antwort = text_completion(openai, TEXT);
-//Antwort = text_correction(openai, TEXT);
-//text_translation(openai, TEXT1,"deutsch")
-//key_validation(openai);
-//image_generation(openai, null);
+//text_completion(TEXT);
+//text_correction(TEXT);
+//text_translation(TEXT1,"deutsch")
+//key_validation();
+//image_generation(null);
